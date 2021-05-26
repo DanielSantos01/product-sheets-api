da4d2805fa5a959630f095367e5092e57aef4d52
Daniel Santos <daniel.ns.santos@gmail.com>
2021-05-26 15:27:59 -0300

    test: 2

--- modules/googlesheet/adapters/GoogleSheet.js
+++ modules/googlesheet/adapters/GoogleSheet.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
 const { google } = require('googleapis');
 
-/*
 const sheet = google.sheets({
     version: 'v4',
     auth: 'AIzaSyDf8tbd8HOz7rmyUnX-sk83L1r2S0sg8R8',
 });
 
 module.exports = sheet;
-*/
